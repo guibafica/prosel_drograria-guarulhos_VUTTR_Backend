5474435bc6cecfd0d204f72b6c41975c3b3a96a8
guibafica <gb.engcomp@hotmail.com>
2021-01-28 15:15:36 -0300

    ADD: Estrutura base

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
